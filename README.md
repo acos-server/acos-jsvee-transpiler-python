081f6f96275e61eefbb5b5218de965041d035066
Teemu Sirkiä <teemu.sirkia@gmail.com>
2016-04-04 21:57:03 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Python transpiler for Jsvee
+
+This tool takes Python 3 code and returns the
+code in format that Jsvee understands. The support
+for different language constructs is limited.
---
+++ index.js
@@ -0,0 +1,66 @@
+var fs = require('fs');
+var python = require('python-shell');
+var htmlencode = require('htmlencode').htmlEncode;
+
+var pythonTranspiler = function() {};
+
+pythonTranspiler.register = function(handlers, app, config) {
+
+  handlers.tools['jsvee-transpiler-python'] = pythonTranspiler;
+
+  app.get('/jsvee-transpiler-python', function(req, res) {
+    res.set('Content-Type', 'text/html');
+    fs.readFile(__dirname + '/form.html', function(err, data) {
+      if (!err) {
+        data = (data + '').replace('{{ url }}', htmlencode(config.serverAddress + 'html/jsvee-python-json/transpiler') + '?url=http://pathtothetranspiledcode/file.json</code>');
+        data = (data + '').replace('{{ kelmu }}', htmlencode(config.serverAddress + 'html/jsvee-python-json/transpiler') + '?url=http://pathtothetranspiledcode/file.json&kelmuUrl=http://pathtothedefinitionfile/file.jsonp</code>');
+        res.send(data);
+      } else {
+        res.send("Error");
+      }
+    });
+  });
+
+  app.post('/jsvee-transpiler-python', function(req, res) {
+    var code = { 'code': req.body.code };
+    var options = {
+      mode: 'json',
+      pythonPath: config.pythonPath || '/usr/bin/python3',
+      pythonOptions: ['-u'], // -u is unbuffered output
+      scriptPath: __dirname
+    };
+
+    var pyshell = new python('parser.py', options);
+    pyshell.send(code);
+
+    var response = '';
+
+    pyshell.on('message', function(message) {
+      response = message;
+    });
+
+    pyshell.end(function(err) {
+      if (err) {
+        res.json({ status: 'ERROR', message: 'Transpilation failed.' });
+      } else {
+        res.json(response);
+      }
+    });
+  });
+
+};
+
+pythonTranspiler.namespace = 'jsvee-transpiler-python';
+pythonTranspiler.packageType = 'tool';
+
+pythonTranspiler.meta = {
+  'name': 'jsvee-transpiler-python',
+  'shortDescription': 'Transpiles the given Python code into form that Jsvee understands.',
+  'description': '',
+  'author': 'Teemu Sirkiä',
+  'license': 'MIT',
+  'version': '0.1.0',
+  'url': ''
+};
+
+module.exports = pythonTranspiler;
